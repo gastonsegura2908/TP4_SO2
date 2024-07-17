a14ac3676236bde1dcac37b5d751876c13b7ea35
Gaston Segura <gastonsegura2908@gmail.com>
2024-07-17 12:48:12 -0300

    pre fix in UART

--- main.c
+++ main.c
@@ -3,20 +3,26 @@
 #include "task.h"
 #include "queue.h"
 
-#include "semphr.h"
-#include "integer.h"
-#include "PollQ.h"
-#include "semtest.h"
-#include "BlockQ.h"
-#include <string.h>
+// #include "semphr.h"
+// #include "integer.h"
+// #include "PollQ.h"
+// #include "semtest.h"
+// #include "BlockQ.h"
+// #include <string.h>
 
 #define ValTempQUEUE_SIZE    (20) // val cola de sensores
-#define mainCHECK_TASK_PRIORITY    (tskIDLE_PRIORITY + 3)
-#define mainCHECK_DELAY    ( ( TickType_t ) 100 / portTICK_PERIOD_MS ) // 0.1 segundos -> Frecuencia 10Hz (1/10)
+//#define mainCHECK_TASK_PRIORITY    (tskIDLE_PRIORITY + 3)
+#define SENSOR_TASK_PRIORITY    (tskIDLE_PRIORITY + 1)
+#define RECEIVER_TASK_PRIORITY  (tskIDLE_PRIORITY + 2)
+#define DISPLAY_TASK_PRIORITY   (tskIDLE_PRIORITY + 3)
+#define mainSENSOR_DELAY    ( ( TickType_t ) 100 / portTICK_PERIOD_MS ) // 0.1 segundos -> Frecuencia 10Hz (1/10)
+#define mainDISPLAY_DELAY   ( ( TickType_t ) 2000 / portTICK_PERIOD_MS ) // 2 segundos
+
 //#define N 5 // Número de mediciones para el filtro pasa bajos
 //#define mainPUSH_BUTTON             GPIO_PIN_4 /* Demo board specifics. */
 #define mainBAUD_RATE				( 19200 ) /* UART configuration - note this does not use the FIFO so is not very efficient. */
 #define mainFIFO_SET					( 0x10 )
+#define timerINTERRUPT_FREQUENCY		( 20000UL )
 
 // Prototipos de las tareas:
 //Configure the processor and peripherals for this demo.
@@ -31,6 +37,10 @@ void updateArray(int[], int, int);
 void reverse(char str[], int length);
 char* itoa(int num, char* str, int base);
 char* bitMapping(int);
+void Timer0IntHandler(void);
+unsigned long getTimerTicks(void);
+void sendUART0(const char *);
+
 
 // Definición de la cola que contiene los valores de temperatura
 QueueHandle_t xValTempQueue;
@@ -38,6 +48,8 @@ QueueHandle_t xValFilterQueue;
 //QueueHandle_t xDisplayQueue;
 
 static int N_filter;
+static volatile char *pcNextChar;
+unsigned long ulHighFrequencyTimerTicks;
 
 #define N_ARRAY    20
 #define MAX_TEMP   30
@@ -59,10 +71,11 @@ int main(void)
     /* Initialise the LCD> */
     OSRAMInit( false );
 
+  	//OSRAMStringDraw("", 16, 1);
     // Crear las tareas
-    xTaskCreate(vTemperatureSensorTask, "TemperatureSensor", configSENSOR_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY - 2, NULL);
-    xTaskCreate(vReceiverTask, "Filter", configFILTER_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY - 2, NULL);
-    xTaskCreate(vDisplayTask, "Display", configDISPLAY_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY - 2, NULL);
+    xTaskCreate(vTemperatureSensorTask, "TemperatureSensor", configSENSOR_STACK_SIZE, NULL, SENSOR_TASK_PRIORITY, NULL);
+    xTaskCreate(vReceiverTask, "Filter", configFILTER_STACK_SIZE, NULL, RECEIVER_TASK_PRIORITY, NULL);
+    xTaskCreate(vDisplayTask, "Display", configDISPLAY_STACK_SIZE, NULL, DISPLAY_TASK_PRIORITY, NULL);
 
     // Iniciar el scheduler de FreeRTOS
     vTaskStartScheduler();
@@ -72,7 +85,7 @@ int main(void)
 }
 
 static void vTemperatureSensorTask(void *pvParameters) {
-    const TickType_t xDelaySensor = mainCHECK_DELAY;
+    //const TickType_t xDelaySensor = mainSENSOR_DELAY;
     int temperature_value = MIN_TEMP;
     while (true) {
         /* Sensor gets temperature value */
@@ -84,7 +97,7 @@ static void vTemperatureSensorTask(void *pvParameters) {
 
         xQueueSend(xValTempQueue, &temperature_value, portMAX_DELAY);
 
-        vTaskDelay(xDelaySensor); // waits mainCHECK_DELAY milliseconds for 10Hz frequency
+        vTaskDelay(mainSENSOR_DELAY); // waits mainSENSOR_DELAY milliseconds for 10Hz frequency
     }
 }
 
@@ -115,8 +128,8 @@ static void vReceiverTask(void *pvParameters) {
 
 static void prvSetupHardware( void )
 {
-	/* Setup the PLL. */
-	//SysCtlClockSet( SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_6MHZ );
+	// Configurar el reloj del sistema para usar el reloj principal
+	SysCtlClockSet( SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_6MHZ );
 
 	/* Setup the push button. */
 	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
@@ -126,14 +139,14 @@ static void prvSetupHardware( void )
 	//GPIOPinIntEnable( GPIO_PORTC_BASE, mainPUSH_BUTTON );
 	//IntEnable( INT_GPIOC );
 
-	/* Enable the UART.  */
+	// Habilitar el puerto UART0
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
 	/* Set GPIO A0 and A1 as peripheral function.  They are used to output the UART signals. */
 	//GPIODirModeSet( GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_DIR_MODE_HW );
 
-	/* Configure the UART for 8-N-1 operation. */
+	/* Configure the UART for 8-N-1 operation.baudios.8 bits de datos, sin paridad y 1 bit de parada */
 	UARTConfigSet( UART0_BASE, mainBAUD_RATE, (UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE ));
 
 
@@ -156,6 +169,12 @@ static void prvSetupHardware( void )
 /*-----------------------------------------------------------*/
 
 static void vDisplayTask(void *pvParameters) {
+	OSRAMStringDraw("SO 2 - TP4", 20, 0);
+    // Esperar 2 segundos
+    vTaskDelay(mainDISPLAY_DELAY);
+    // Limpiar el display
+    OSRAMClear();
+
 	/* create array with latest filtered values */
   	int filtered_array[DISPLAY_COLUMNS] = {};
 
@@ -295,15 +314,88 @@ char* bitMapping(int valor) {
 }
 
 
-void vUART_ISR(void) {
-    // ISR de UART
+
+void vUART_ISR(void){
+	unsigned long ulStatus;
+
+	/* What caused the interrupt. */
+	ulStatus = UARTIntStatus( UART0_BASE, pdTRUE );
+
+	/* Clear the interrupt. */
+	UARTIntClear( UART0_BASE, ulStatus );
+
+	/* Was a Tx interrupt pending? */
+	if( ulStatus & UART_INT_TX )
+	{
+		/* Send the next character in the string.  We are not using the FIFO. */
+		if( *pcNextChar != 0 )
+		{
+			if( !( HWREG( UART0_BASE + UART_O_FR ) & UART_FR_TXFF ) )
+			{
+				HWREG( UART0_BASE + UART_O_DR ) = *pcNextChar;
+			}
+			pcNextChar++;
+		}
+	}
+	/* Was a Rx interrupt pending? */
+    else if (ulStatus & UART_INT_RX) {
+        signed char input;
+        portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+
+        /* A character was received.  Place it in the queue of received characters. */
+        input = UARTCharGet(UART0_BASE);
+        if (input == '+' && getN() < N_ARRAY) {
+            setN(getN() + 1);
+			sendUART0("\r\nN incremented\r\n");
+        }
+		else if(input == '-' && getN() > 1){
+			setN(getN() - 1);
+			sendUART0("\r\nN decremented\r\n");
+		}
+		else {
+			sendUART0("\r\nCan't permform that operation on N, limit reached \r\n");
+		}
+        portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+    }
 }
 
+/* Send and print string through UART0 */
+void sendUART0(const char *string) {
+	while (*string != '\0') {
+		UARTCharPut(UART0_BASE, *string);
+		string++;
+	}
+}
+
+/* Timer0 set up and configuration */
 void setupTimer0(void) {
-    // Configuración del Timer0
+	/* Enable Timer0 */
+  	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+  	TimerConfigure(TIMER0_BASE, TIMER_CFG_32_BIT_TIMER);
+
+	/* Ensure interrupts do not start until the scheduler is running. */
+	portDISABLE_INTERRUPTS();
+
+	/* Allow global interrupt */
+	IntMasterEnable();
+  	
+	/* Set Timer0 interrupt */
+	unsigned long ulFrequency = configCPU_CLOCK_HZ / timerINTERRUPT_FREQUENCY;
+  	TimerLoadSet(TIMER0_BASE, TIMER_A, ulFrequency);
+  	TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
+	TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+	/* Enable Timer0 */
+  	TimerEnable(TIMER0_BASE, TIMER_A);
 }
 
+/* Timer0 Interrupt Handler */
+void Timer0IntHandler(void) {
+	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+	ulHighFrequencyTimerTicks++;
+}
+
+/* Get timer Ticks */
 unsigned long getTimerTicks(void) {
-    // Obtener ticks del timer
-    return 0; // ulHighFrequencyTimerTicks;
+  	return ulHighFrequencyTimerTicks;
 }
